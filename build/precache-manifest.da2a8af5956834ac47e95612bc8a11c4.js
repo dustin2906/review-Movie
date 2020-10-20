597a735c1684a0ba36eb405fd232a8f2d604314a
= <=>
2020-10-20 18:47:49 +0300

    first commit

---
+++ build/precache-manifest.da2a8af5956834ac47e95612bc8a11c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ed1bd5548b0a3ba1c5024d6a0b47e7",
+    "url": "/review-Movie/index.html"
+  },
+  {
+    "revision": "ac674b7f42e6227a8ed3",
+    "url": "/review-Movie/static/css/main.0b6be90e.chunk.css"
+  },
+  {
+    "revision": "486da8e27f0ffa76b7cb",
+    "url": "/review-Movie/static/js/2.4c3a15a0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/review-Movie/static/js/2.4c3a15a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac674b7f42e6227a8ed3",
+    "url": "/review-Movie/static/js/main.839ae129.chunk.js"
+  },
+  {
+    "revision": "4cbc4e523a0f70e1b204",
+    "url": "/review-Movie/static/js/runtime-main.75a2d6f3.js"
+  }
+]);
